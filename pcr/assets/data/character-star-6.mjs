69e73084d83b7d546cddb8fe144f0ee493dcdeb0
aoisummer <aoisummer@users.noreply.github.com>
2020-11-27 18:26:36 +0800

    Update pcr

--- pcr/assets/data/character-star-6.mjs
+++ pcr/assets/data/character-star-6.mjs
@@ -19,4 +19,5 @@ export default [
     { cid: 1032, star6DateCN: null, star6DateJP: "2020-09-07" },
     { cid: 1030, star6DateCN: null, star6DateJP: "2020-10-07" },
     { cid: 1033, star6DateCN: null, star6DateJP: "2020-11-05" },
+    { cid: 1016, star6DateCN: null, star6DateJP: "2020-12-07" },
 ]
--- pcr/assets/quest-storyevent.mjs
+++ pcr/assets/quest-storyevent.mjs
@@ -1,44 +1,67 @@
 import Navbar from './component/navbar.mjs';
 
 import characterCoreData from './data/character-core.mjs';
-import storyeventData from './data/quest-storyevent.mjs';
+import storyeventDataJP from './data/quest-storyevent-jp.mjs';
+import storyeventDataCN from './data/quest-storyevent-cn.mjs';
 
 const contentData = (() => {
     const map = new Map();
     characterCoreData.forEach((item) => {
         map.set(item.id, item);
     });
-    return storyeventData.map((item) => {
-        return { ...item, character: item.character.map((item2) => map.get(item2).name) };
-    });
+    const parseData = (arr) => {
+        return arr.map((item) => {
+            return { ...item, character: item.character.map((item2) => map.get(item2).name) };
+        })
+    };
+    return [parseData(storyeventDataCN), parseData(storyeventDataJP)];
 })();
 
 function DataList({ data }) {
     return (
         <ul className="list-group">
-            { data.map((item, index) => {
-                const dateField = item.startDate.split('T')[0] + ' +' + Math.round((new Date(item.endDate) - new Date(item.startDate)) / 1000 / 86400) + 'd';
-                return (
-                    <li className="list-group-item" key={index}>
-                        <div className="row">
-                            <div className="col-sm-6" title="活动名称">{item.name}</div>
-                            <div className="col-sm-4" title="角色碎片">{item.character.join('、')}</div>
-                            <div className="col-sm-2 text-right text-secondary" title="活动日期">{dateField}</div>
-                        </div>
-                    </li>
-                );
-            }) }
+        { data.map((item, index) => {
+            const dateField = item.startDate.split('T')[0] + ' +' + Math.round((new Date(item.endDate) - new Date(item.startDate)) / 1000 / 86400) + 'd';
+            return (
+                <li className="list-group-item" key={index}>
+                    <div className="row">
+                        <div className="col-sm-6" title="活动名称">{item.name}</div>
+                        <div className="col-sm-4" title="角色碎片">{item.character.join('、')}</div>
+                        <div className="col-sm-2 text-right text-secondary" title="活动日期">{dateField}</div>
+                    </div>
+                </li>
+            );
+        }) }
         </ul>
     );
 }
 
+function Tabs({ current, onTabClick }) {
+    const tabs = ['简体字版', '日版'];
+    return (
+        <div className="x-tabs-nav x-tabs-nav-block mb-3">
+        { tabs.map((item, index) => {
+            const className = ['x-tabs-nav-item'];
+            current === index && className.push('active');
+            return <a className={className.join(' ')} href="#" data-index={index} key={index} onClick={onTabClick}>{item}</a>
+        }) }
+        </div>
+    );
+}
+
 function App() {
+    const [filterIndex, setFilterIndex] = React.useState(1);
+    const handleFilterClick = (e) => {
+        e.preventDefault();
+        setFilterIndex(Number(e.currentTarget.getAttribute('data-index')));
+    };
+
     return (
         <div className="container my-3">
             <Navbar />
             <h2 className="h5 mb-3">剧情活动一览</h2>
-            <DataList data={contentData} />
-            <div className="mt-2 text-secondary x-text-sm">※ 以日版作为基础数据，若简体字版已开展过则替换为对应名字</div>
+            <Tabs current={filterIndex} onTabClick={handleFilterClick} />
+            <DataList data={contentData[filterIndex]} />
         </div>
     );
 }
