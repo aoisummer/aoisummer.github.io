3d3a3eae1d3c2c8b690bef7600c6f118709c3f55
aoisummer <aoisummer@users.noreply.github.com>
2022-08-26 00:34:34 +0800

    update calendar

--- calendar/main.js
+++ calendar/main.js
@@ -1,3 +1,5 @@
+const { useState, useEffect, useCallback, useRef } = window.React;
+
 function Sidebar({ sources, onSourceClick }) {
     return (
         <div className="x-sidebar">
@@ -17,10 +19,10 @@ function Sidebar({ sources, onSourceClick }) {
 }
 
 function Calendar({ sources }) {
-    const calEl = React.useRef(null);
-    const _calendar = React.useRef(null);
+    const calEl = useRef(null);
+    const _calendar = useRef(null);
 
-    React.useEffect(() => {
+    useEffect(() => {
         _calendar.current = new FullCalendar.Calendar(calEl.current, {
             locale: 'zh-cn',
             displayEventEnd: true,
@@ -33,7 +35,7 @@ function Calendar({ sources }) {
         };
     }, []);
 
-    React.useEffect(() => {
+    useEffect(() => {
         const oldArr = _calendar.current.getEventSources().map((es) => es.id);
         const newArr = sources.filter((es) => es._checked).map((es) => es.id);
         const removeArr = oldArr.filter((id) => newArr.indexOf(id) === -1);
@@ -53,15 +55,15 @@ function Calendar({ sources }) {
 }
 
 function App() {
-    const [sources, setSources] = React.useState([]);
-    const onSourceClick = (e) => {
+    const [sources, setSources] = useState([]);
+    const onSourceClick = useCallback((e) => {
         const id = e.target.value;
         setSources((prevState) => {
             return prevState.map((es) => es.id === id ? { ...es, _checked: !es._checked } : es);
         });
-    };
+    }, []);
 
-    React.useEffect(() => {
+    useEffect(() => {
         const group = new URLSearchParams(location.search).get('group');
         setSources(getSourceGroup(group || 'pcrcn'));
     }, []);
@@ -88,7 +90,12 @@ function getSourceGroup(name) {
     };
     return sources.hasOwnProperty(name) ? sources[name].map((item) => {
         // return { ...item, url: `data/${item.id}.json`, _checked: true };
-        return { ...item, url: `https://cdn.jsdelivr.net/gh/aoisummer/ics-collection@master/dist/${item.id}.json`, _checked: true };
+        return {
+            ...item,
+            url: `https://cdn.jsdelivr.net/gh/aoisummer/ics-collection/dist/${item.id}.json`,
+            extraParams: () => ({ _: Math.floor(Date.now() / 1000 / 86400) }),
+            _checked: true,
+        };
     }) : [];
 };
 
